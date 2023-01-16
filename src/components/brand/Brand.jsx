4bdd31e44082fb4da4adcdc2dce995f270633936
Andriy Panasiuk <panasiukmail@gmail.com>
2023-01-16 20:24:31 +0200

    feat: Gpt3 and Feature component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.scss';
 
-const Brand = () => {
-    return (
-        <div>Brand</div>
-    )
-}
-
+const Brand = () => (
+    <div className="gpt3__brand section__padding">
+        <div className='gpt3__brand__item'>
+            <img src={google} />
+        </div>
+        <div className='gpt3__brand__item'>
+            <img src={slack} />
+        </div>
+        <div className='gpt3__brand__item'>
+            <img src={atlassian} />
+        </div>
+        <div className='gpt3__brand__item'>
+            <img src={dropbox} />
+        </div>
+        <div className='gpt3__brand__item'>
+            <img src={shopify} />
+        </div>
+    </div>
+);
 export default Brand
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/companies/google.png';
+import slack from '../../assets/companies/slack.png';
+import atlassian from '../../assets/companies/atlassian.png';
+import dropbox from '../../assets/companies/dropbox.png';
+import shopify from '../../assets/companies/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+};
